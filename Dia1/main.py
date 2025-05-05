b530560e7c64861ce546083a5dc7c234423d5e6f
Luis Gelvez <luisangelgelvezdelgado1750@gmail.com>
2025-05-05 12:59:23 -0500

    feat: :sparkles: adding day 1

---
+++ Dia1/main.py
@@ -0,0 +1,72 @@
+
+ # ##########################
+ # #### Clase Dia 1 ######
+ # ##########################
+ 
+ #Imprimir en Pitón
+ print("Hola Mundo!")
+ 
+ #Creación de variables
+ #1. Dato tipo string
+ nombre = "Luis Angel"
+ print(type(nombre))
+ #2. Dato tipo numero entero
+ numeroEntero=5
+ print(numeroEntero)
+ print(type(numeroEntero))
+ #3. Dato tipo numero real
+ numeroReal = 5.3
+ print(type(numeroReal))
+ #4. Dato tipo double
+ numeroPi=3.14163456789098765434567890
+ print(numeroPi)
+ #5. Dato tipo booleano
+ booleanito = True
+ print(booleanito)
+ #6. (SOLO PYTHON) Numero complejo
+ numeroComplejo= complex('+1.23')
+ print(numeroComplejo)
+ print(type(numeroComplejo))
+ # ##########################
+ 
+ #Convertir numero int a float
+ numeroNuevo = float(numeroEntero)
+ print(numeroNuevo)
+ print(" ")
+ 
+ # ##########################
+ #Ciclo for (Hasta)
+ for i in range(5):
+     print(i)
+ print(" ")
+ #Ciclo for (Desde- Hasta)
+ for i in range(0,5):
+     print(i)
+ print("")
+ for i in range(1,5):
+     print(i)
+ print("")
+ #Ciclo for (Desde-Hasta-Paso)
+ for i in range(1,5,1):
+     print(i)
+ # ##########################
+ #Ciclo While
+ booleanitoNuevo = True
+ while(booleanitoNuevo==True):
+     print("Sigo siendo verdadero!!!!!")
+     booleanitoNuevo=False
+ 
+ booleanitoNuevo = True
+ while(booleanitoNuevo):
+     print("Sigo siendo verdadero!!!!!")
+     booleanitoNuevo=False
+ 
+ print(" ")
+ # ##########################
+ # Condicionales if-else
+ texto="Luis Angel"
+ if(texto=="Corpus"):
+     print("Sos corpus :yay:")
+
+     ###desarrollado por: Luis Angel Gelvez Delgado cc.1.098.071.730
+ 
